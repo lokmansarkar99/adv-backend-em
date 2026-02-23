b730bca478a928b9a01c5e234ed0c7f6d4932795
Lokman Sarkar <=>
2026-02-23 15:58:54 +0600

    changes in controller for token

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -6,6 +6,8 @@ import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { AuthService } from "./auth.service";
 
+import config from "../../../config";
+
 const registerUser = catchAsync(async(req:Request, res:Response) => {
     const result = await AuthService.registerToDB(req.body)
     sendResponse(res, {
@@ -19,6 +21,15 @@ const registerUser = catchAsync(async(req:Request, res:Response) => {
 
 const loginUser = catchAsync(async(req:Request, res:Response) => {
     const result = await AuthService.logintoDB(req.body)
+
+    const {accessToken, refeshToken} = await result
+res.cookie("accessToken", accessToken, {
+    httpOnly:true,
+    sameSite: "lax",
+    secure: config.node_env ==="production",
+    maxAge: 15 * 60 * 1000
+})
+
 
     sendResponse(res, {
         success:true,
